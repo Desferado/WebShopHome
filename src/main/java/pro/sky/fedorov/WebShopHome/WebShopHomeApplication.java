944b8b635a16fecbd7ffe2885cef2a46020de5ad
Denis_vrn <denisfedorov50@gmail.com>
2023-05-05 23:35:41 +0300

    Refactoring....

--- src/main/java/pro/sky/fedorov/WebShopHome/WebShopHomeApplication.java
+++ src/main/java/pro/sky/fedorov/WebShopHome/WebShopHomeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebShopHomeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WebShopHomeApplication.class, args);
 	}
 
