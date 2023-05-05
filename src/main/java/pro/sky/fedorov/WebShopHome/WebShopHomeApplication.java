edf4db611c16c41e523a591e4212b1a645e3ade9
Denis_vrn <denisfedorov50@gmail.com>
2023-05-05 23:34:46 +0300

    Initial commit

---
+++ src/main/java/pro/sky/fedorov/WebShopHome/WebShopHomeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.fedorov.WebShopHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopHomeApplication.class, args);
+	}
+
+}
